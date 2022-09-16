5e6178a1f2690e0c1c0d34f8b4ec72d4d7d67d30
Sorin Iordache <jordache_sor@yahoo.com>
2022-09-16 18:18:19 +0300

    Add hero details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +21,5 @@ import { HeroesComponent } from './heroes/heroes.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
